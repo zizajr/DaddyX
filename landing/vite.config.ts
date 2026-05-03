fcf7744023bb60bdf2209b5365543c55168f0425
zizajr <77775771+zizajr@users.noreply.github.com>
2026-05-03 10:15:46 +0200

    feat(landing): add vite.config.ts

---
+++ landing/vite.config.ts
@@ -0,0 +1,17 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
+});
